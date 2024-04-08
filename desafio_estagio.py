f69e686210061147941bd3599148b619e5cb009a
TortugoBell <vitorhsilveira@gmail.com>
2024-04-08 09:14:16 -0300

    Create desafio_estagio.py
    Verificador em python

---
+++ desafio_estagio.py
@@ -0,0 +1,21 @@
+def fibonacci(n):
+    a, b = 0, 1
+    while b < n:
+        a, b = b, a + b
+    return b == n
+
+def main():
+    while True:
+        numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci (ou digite 0 para sair): "))
+        
+        if numero == 0:
+            print("Encerrando o programa...")
+            break
+        
+        if fibonacci(numero):
+            print(f"O número {numero} pertence à sequência de Fibonacci.")
+        else:
+            print(f"O número {numero} não pertence à sequência de Fibonacci.")
+
+if __name__ == "__main__":
+    main()
